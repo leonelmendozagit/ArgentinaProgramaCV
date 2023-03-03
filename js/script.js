b4b9ec899988e7f4409887e6c0646a9b363679b6
leonelmendozagit <114789609+leonelmendozagit@users.noreply.github.com>
2023-03-02 23:42:07 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function MensajeEnviado() {
     window.alert("Su mensaje fue enviado con éxito, brevemente nos comunicaremos con usted a través del correo que nos ha proporcionado. :)")
     
-    let comprobanteDeFormulario = document.getElementById("enviado").ariaValueMax;
+    let comprobanteDeFormulario = document.getElementById("enviado").value;
 
     if (comprobanteDeFormulario != "") {
         document.getElementById('enviado').style.backgroundColor = "green";
